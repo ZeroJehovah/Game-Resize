a57f9464bc39be81b24298ed8319a4a5815f2d12
ZeroJehovah <zj294753618@gmail.com>
2023-05-16 08:55:19 +0800

    仅允许单实例

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace Game_Resize
         [STAThread]
         static void Main()
         {
+            Mutex mutex = new Mutex(true, "Game-Resize");
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                // 已经有一个实例在运行，退出当前实例
+                MessageBox.Show("程序已经在运行了", "提示");
+                return;
+            }
+
             Console.WriteLine("Application started");
 
             Application.EnableVisualStyles();
